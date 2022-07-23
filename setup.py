b50cf1c8382db7603617f45038e2dd97689f5efd
Daniel Strano <stranoj@gmail.com>
2022-07-23 01:13:48 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = [
 ]
 
 # Handle version.
-VERSION = "0.1.1"
+VERSION = "0.1.3"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
